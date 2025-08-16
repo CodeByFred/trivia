6c8152f305065d64d8d59dd28b8ccf77799a7367
FJack <fredlishiajack@gmail.com>
2025-08-16 13:18:36 +1000

    add types | implement fetch from trivia api | turn off strict mode for dev

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
+import { useEffect, useState } from "react";
+import { triviaQuery } from "./services/triviaHttp";
+import type { Question } from "./types/types";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const data = await triviaQuery(2, "easy", 9);
+        setQuestions(data);
+      } catch {
+        setError("Something went wrong.");
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
+
+  if (loading) return <p>Loading questions...</p>;
+  if (error) return <p>{error}</p>;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {questions.map((q, i) => (
+        <div key={i}>
+          <h3>{q.question}</h3>
+          <ul>
+            {[q.correctAnswer, ...q.incorrectAnswers].map((ans, index) => (
+              <li key={index}>{ans}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
