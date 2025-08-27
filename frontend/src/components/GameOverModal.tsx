70e2eb6c9a7a295100905f6bfb7555acd85472b8
carriegale2710 <carriegale2710@gmail.com>
2025-08-27 13:26:04 +1000

    hooks states from context into GamePage components

--- frontend/src/components/GameOverModal.tsx
+++ frontend/src/components/GameOverModal.tsx
@@ -1,11 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import Button from "./Button";
+import { useGameContext } from "../context/useGameContext";
 
 const GameOverModal = ({ resetGame }: { resetGame: () => void }) => {
+  const navigate = useNavigate();
+
+  const { score, questions } = useGameContext();
+
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-gray-800 bg-opacity-50">
       <div className="bg-white p-4 rounded shadow-md">
         <span className="flex flex-col text-center  ">
           <h1>Game Over</h1>
+          <p>
+            Final score: {score}/{questions.length}
+          </p>
           <p>Thank you for playing!</p>
         </span>
         <span className="flex flex-col space-y-4 my-4">
