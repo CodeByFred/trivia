2e44daf09f6b19d7f2b0e63d4f2fbdb5493ad2c3
carriegale2710 <carriegale2710@gmail.com>
2025-08-28 23:08:43 +1000

    feat: adds getAllQuestions in QuestionController

--- src/main/java/io/nology/trivia/question/QuestionController.java
+++ src/main/java/io/nology/trivia/question/QuestionController.java
@@ -1,4 +1,26 @@
 package io.nology.trivia.question;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/questions")
 public class QuestionController {
+
+    private final QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        List<Question> questions = questionService.findAll();
+        return ResponseEntity.ok(questions);
+    }
+
 }
--- src/main/java/io/nology/trivia/question/QuestionService.java
+++ src/main/java/io/nology/trivia/question/QuestionService.java
@@ -1,4 +1,20 @@
 package io.nology.trivia.question;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    public List<Question> findAll() {
+        return questionRepository.findAll();
+    }
+
 }
