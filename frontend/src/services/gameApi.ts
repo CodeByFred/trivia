53750bf3146a09436afe6b2c0ea19be2c975f86b
carriegale2710 <carriegale2710@gmail.com>
2025-08-29 00:40:01 +1000

    refactor: adds error-handling to api post method and updates dto type

--- frontend/src/services/gameApi.ts
+++ frontend/src/services/gameApi.ts
@@ -1,23 +1,25 @@
-import type { GameData } from "../types/types";
+import type { GameResult } from "../types/types";
 import { API, GAME_ENDPOINT } from "./urls";
 
-export async function submitGameData(gameData: GameData) {
+export async function submitGameResult(gameResultDto: GameResult) {
   try {
     const response = await fetch(`${API}${GAME_ENDPOINT}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(gameData),
+      body: JSON.stringify(gameResultDto),
     });
 
     if (!response.ok) {
-      console.log(response);
+      console.error(response);
+      throw new Error("Failed to submit game result");
     }
 
-    const result = response.text;
+    const result = await response.text();
     return result;
-  } catch (reponse) {
-    console.log(reponse);
+    //return the confirmation message from backend
+  } catch (error) {
+    console.error(error);
   }
 }
