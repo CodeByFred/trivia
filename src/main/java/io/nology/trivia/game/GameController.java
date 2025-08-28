c2b15dccf7387188fd7a0df0c33d0bd6dc0ec5f0
carriegale2710 <carriegale2710@gmail.com>
2025-08-28 23:08:21 +1000

    feat: add saveGame endpoint to GameController

--- src/main/java/io/nology/trivia/game/GameController.java
+++ src/main/java/io/nology/trivia/game/GameController.java
@@ -1,4 +1,29 @@
 package io.nology.trivia.game;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.nology.trivia.game.entities.Game;
+
+@Controller
+@RequestMapping("/game")
 public class GameController {
+
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    // Add endpoints for game-related actions
+
+    @PostMapping("/save")
+    public ResponseEntity<Game> saveGame(@RequestBody Game game) {
+        Game newGame = gameService.saveGame(game);
+        return ResponseEntity.ok(newGame);
+    }
+
 }
--- src/main/java/io/nology/trivia/game/GameService.java
+++ src/main/java/io/nology/trivia/game/GameService.java
@@ -1,4 +1,20 @@
 package io.nology.trivia.game;
 
+import org.springframework.stereotype.Service;
+
+import io.nology.trivia.game.entities.Game;
+
+@Service
 public class GameService {
+
+    private GameRepository gameRepository;
+
+    public GameService(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public Game saveGame(Game game) {
+        return gameRepository.save(game);
+    }
+
 }
