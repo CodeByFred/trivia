62e08f6a4c70c6ec536be84f83cb2f5a188b20b6
carriegale2710 <carriegale2710@gmail.com>
2025-09-01 17:00:04 +1000

    deletes correctAnswer display

--- frontend/src/components/GameLog.tsx
+++ frontend/src/components/GameLog.tsx
@@ -1,12 +1,6 @@
 import type { GameResult } from "../types/types";
 
-const GameLog = ({
-  game,
-  showingAnswers,
-}: {
-  game: GameResult;
-  showingAnswers: boolean;
-}) => {
+const GameLog = ({ game }: { game: GameResult }) => {
   return (
     <div className="border p-4 m-4 rounded-md shadow-md ">
       <h2 className="p-2 bg-black text-white text-center">
@@ -17,13 +11,7 @@ const GameLog = ({
           <p className="font-bold ">{game.questions[i].question}</p>
           <div className=" flex flex-row gap-4">
             <p>Your answer: {entry.submitted}</p>
-            <p className={entry.wasCorrect ? "text-green-500" : "text-red-500"}>
-              {entry.wasCorrect ? "(Correct)" : "(Incorrect)"}
-            </p>
           </div>
-          {showingAnswers && entry.wasCorrect === false && (
-            <p>Correct answer: {game.questions[i].correctAnswer}</p>
-          )}
         </div>
       ))}
     </div>
--- frontend/src/pages/ReviewPage.tsx
+++ frontend/src/pages/ReviewPage.tsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
 import { useGameContext } from "../context/useGameContext";
 import GameLog from "../components/GameLog";
 
 const ReviewPage = () => {
   const { gameHistory } = useGameContext();
-  const [showingAnswers, setShowingAnswers] = useState(false);
 
   return (
     <div className="mx-auto items-center flex flex-col gap-4 p-4 h-full overflow-y-auto">
@@ -12,21 +10,12 @@ const ReviewPage = () => {
         <h1>Review Your Answers</h1>
         <p>Here are the questions you answered:</p>
       </div>
-      <label className="flex items-center gap-2">
-        <input
-          type="checkbox"
-          onClick={() => setShowingAnswers(!showingAnswers)}
-        />
-        Show Correct Answers
-      </label>
       <div className="overflow-clip  ">
         {gameHistory.length === 0 && (
           <p>No answers to review. Start a new game!</p>
         )}
         {gameHistory.length > 0 &&
-          gameHistory.map((game, i) => (
-            <GameLog key={i} game={game} showingAnswers={showingAnswers} />
-          ))}
+          gameHistory.map((game, i) => <GameLog key={i} game={game} />)}
       </div>
     </div>
   );
