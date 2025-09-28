bf9777662c844f7d5b4acaf5a5de06911da55268
FJack <fredlishiajack@gmail.com>
2025-09-28 15:38:32 +1000

    adjust cors configuration to accept patch requests

--- src/main/java/io/nology/trivia/config/CorsConfig.java
+++ src/main/java/io/nology/trivia/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("GET", "POST", "PUT")
+        registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("GET", "POST", "PUT", "PATCH")
                 .allowedHeaders("*");
     }
 }
