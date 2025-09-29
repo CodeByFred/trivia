1c4db0acfb371632007fe16f26b2698a61e8752a
carriegale2710 <carriegale2710@gmail.com>
2025-09-29 17:48:21 +1000

    deletes reviewPage (gameHistory)

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -14,9 +14,6 @@ const NavBar = () => {
           <li className="text-xl">
             <NavLink to="/game">Game</NavLink>
           </li>
-          {/* <li className="text-xl">
-            <NavLink to="/review">Review</NavLink>
-          </li> */}
           <li className="text-xl">
             <NavLink to="/retry">Retry</NavLink>
           </li>
