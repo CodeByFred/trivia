eb361732ee2f4e30ad9092275df3ac51aa9584b9
carriegale2710 <carriegale2710@gmail.com>
2026-09-03 00:05:46 +1000

    change yo local api server url for frontend

--- frontend/src/services/urls.ts
+++ frontend/src/services/urls.ts
@@ -1,4 +1,5 @@
-export const API = "https://api.mytriviagame.lol"; // change to "http://localhost:8080" for local API server
+// export const API = "https://api.mytriviagame.lol"; // change to "http://localhost:8080" for local API server
+export const API = "http://localhost:8080";
 export const GAME_ENDPOINT = "/games";
 export const GAMEANSWER_ENDPOINT = "/game-answers";
 export const QUESTION_ENDPOINT = "/questions";
