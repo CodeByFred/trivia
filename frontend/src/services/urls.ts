349911b38aca4a4dd75757909de36c76f4a12a44
carriegale2710 <carriegale2710@gmail.com>
2026-09-03 00:30:10 +1000

    fix: api url typo

--- frontend/src/services/urls.ts
+++ frontend/src/services/urls.ts
@@ -1,5 +1,7 @@
 export const rawUrl = import.meta.env.VITE_API_URL;
-export const API = rawUrl ? `https://${rawUrl}` : "http://localhost:8080";
+export const API = rawUrl
+  ? `https://${rawUrl}.onrender.com`
+  : "http://localhost:8080";
 export const GAME_ENDPOINT = `/games`;
 export const GAMEANSWER_ENDPOINT = `/game-answers`;
 export const QUESTION_ENDPOINT = `/questions`;
