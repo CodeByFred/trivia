2ad3b3a40ae7801eb34c172cbb9aab05e5d52d57
carriegale2710 <carriegale2710@gmail.com>
2026-09-03 00:16:48 +1000

    fix: api variable name mismatch

--- frontend/src/services/urls.ts
+++ frontend/src/services/urls.ts
@@ -1,5 +1,5 @@
 export const rawUrl = import.meta.env.VITE_API_URL;
-export const API_URL = rawUrl ? `https://${rawUrl}` : "http://localhost:8080";
-export const GAME_ENDPOINT = `${API_URL}/games`;
-export const GAMEANSWER_ENDPOINT = `${API_URL}/game-answers`;
-export const QUESTION_ENDPOINT = `${API_URL}/questions`;
+export const API = rawUrl ? `https://${rawUrl}` : "http://localhost:8080";
+export const GAME_ENDPOINT = `/games`;
+export const GAMEANSWER_ENDPOINT = `/game-answers`;
+export const QUESTION_ENDPOINT = `/questions`;
--- src/main/java/io/nology/trivia/config/CorsConfig.java
+++ src/main/java/io/nology/trivia/config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig implements WebMvcConfigurer {
                         "http://localhost:5173",
                         "http://120.0.0.1:5173",
                         "https://mytriviagame.lol",
-                        "https://www.mytriviagame.lol")
+                        "https://www.mytriviagame.lol",
+                    )
                 .allowedMethods("GET", "POST", "PUT", "PATCH")
                 .allowedHeaders("*");
     }
